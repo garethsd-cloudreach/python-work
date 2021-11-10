4e09ecc2c4435f8571d169182fcf1d5c4ae88d3c
Gareth Scott-Dodd <garethscott-dodd@Gareths-MacBook-Pro.local>
2021-11-10 18:09:27 +0000

    python day one extra

--- apples.py
+++ apples.py
@@ -1,10 +1,10 @@
 apple = int((input) ("How many apples do you have?"))
 weight = str((input) ("Kg or Lbs?"))
 
-Kg = 5 
+Kg = 0.2 
 Lbs = 0.44
 
-if weight == Kg:
+if weight == "Kg":
     print(apple * Kg)
 else:
      print(apple * Lbs)
